8268c6691114dd7adb20ec44877f47a1310a5ce9
SAHILDWIVEDI1212 <saahil15422@gmail.com>
2026-07-22 16:24:18 +0530

    fix cors configuration

--- src/app.js
+++ src/app.js
@@ -47,31 +47,77 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth.routes');
-const foodRoutes = require("./routes/food.routes");
-const foodPartnerRoutes = require("./routes/food-partner.routes");
+const foodRoutes = require('./routes/food.routes');
+const foodPartnerRoutes = require('./routes/food-partner.routes');
 
 const app = express();
+
 
 const allowedOrigins = [
   "http://localhost:5173",
   "https://vercel-frontend-eight-brown.vercel.app"
 ];
 
+
+// CORS CONFIG
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
+  origin: function(origin, callback){
+
+    // allow postman / server requests
+    if(!origin){
+      return callback(null, true);
+    }
+
+    if(allowedOrigins.includes(origin)){
+      return callback(null, true);
+    }
+
+    return callback(new Error("CORS Error: Origin not allowed"));
+  },
+
+  credentials: true,
+
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "PATCH",
+    "OPTIONS"
+  ],
+
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization"
+  ]
 }));
+
+
+// handle preflight request
+app.options("*", cors());
+
 
 app.use(cookieParser());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+
+// test route
+app.get("/", (req,res)=>{
+    res.send("Hello World");
 });
 
+
+
+// routes
 app.use('/api/auth', authRoutes);
+
 app.use('/api/food', foodRoutes);
+
 app.use('/api/food-partner', foodPartnerRoutes);
 
+
+
 module.exports = app;
+
